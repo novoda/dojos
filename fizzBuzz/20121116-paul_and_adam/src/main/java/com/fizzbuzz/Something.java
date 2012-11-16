8a45ee607bf63f3178be27218be7d2afe0725ff2
Blundell <paul@novoda.com>
2012-11-16 18:24:03 +0000

    incorporated 0 into test harness

--- fizzBuzz/20121116-paul_and_adam/src/main/java/com/fizzbuzz/Something.java
+++ fizzBuzz/20121116-paul_and_adam/src/main/java/com/fizzbuzz/Something.java
@@ -4,7 +4,7 @@ public class Something {
 	
 	public static void main(String[] args){
 		FizzBuzzPrinter fizzBuzzPrinter = new FizzBuzzPrinter();
-		for (int i = 1; i < 101; i++) {
+		for (int i = 0; i < 101; i++) {
 			System.out.println(fizzBuzzPrinter.print(i));
 		}
 	}
