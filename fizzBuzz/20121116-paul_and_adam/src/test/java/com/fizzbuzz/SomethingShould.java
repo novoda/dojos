192a078f74e7c01709d203b5c0126efb4823b1d9
Blundell <paul@novoda.com>
2012-11-16 17:24:44 +0000

    first test

--- fizzBuzz/20121116-paul_and_adam/src/test/java/com/fizzbuzz/SomethingShould.java
+++ fizzBuzz/20121116-paul_and_adam/src/test/java/com/fizzbuzz/SomethingShould.java
@@ -1,13 +1,18 @@
 package com.fizzbuzz;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class SomethingShould{
 
     @Test
-    public void something() {
-    	fail();
+    public void returnTrueIfNumberIs9andMultipleIs3() {
+    	assertTrue(isMultipleOf(3, 9));
     }
+    
+    private boolean isMultipleOf(int multiple, int number) {
+    	return (number % multiple) == 0;
+    }
+    
 }
