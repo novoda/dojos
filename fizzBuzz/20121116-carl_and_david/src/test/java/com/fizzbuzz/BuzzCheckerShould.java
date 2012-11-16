3e7e63ea5928d0ca315ff0991901b0076f770999
David Gonzalez <malmstein@gmail.com>
2012-11-16 17:36:39 +0000

    add BuzzChecker

---
+++ fizzBuzz/20121116-carl_and_david/src/test/java/com/fizzbuzz/BuzzCheckerShould.java
@@ -0,0 +1,13 @@
+package com.fizzbuzz;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class BuzzCheckerShould {
+
+	@Test
+	public void ensure_multiple_of_5_are_checked(){
+		assertTrue(true);
+	}
+}
