940d71b95edacb171033fc16ca1174804cf0df0c
Peter <peter.budo@gmail.com>
2012-11-16 17:27:00 +0000

    adding test for 2

--- fizzBuzz/20121116-goncalo_and_peter/src/test/java/com/fizzbuzz/SomethingShould.java
+++ fizzBuzz/20121116-goncalo_and_peter/src/test/java/com/fizzbuzz/SomethingShould.java
@@ -7,11 +7,16 @@ import static junit.framework.Assert.assertEquals;
 public class SomethingShould{
 
     @Test
-    public void something() {
-        assertEquals("", fizzBuzz(1));
+    public void print_one_as_a_number() {
+        assertEquals("1", fizzBuzz(1));
+    }
+
+    @Test
+    public void print_two_as_a_number() {
+        assertEquals("2", fizzBuzz(2));
     }
 
     private String fizzBuzz(int i) {
-        return "";  //To change body of created methods use File | Settings | File Templates.
+        return ""+i;  //To change body of created methods use File | Settings | File Templates.
     }
 }
