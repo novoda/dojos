12c17b10ef8db3d6e6f6ab19b975e53f3671dcc6
Blundell <paul@novoda.com>
2012-11-16 17:47:08 +0000

    file

---
+++ fizzBuzz/20121116-paul_and_adam/src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,22 @@
+package com.fizzbuzz;
+
+import static com.fizzbuzz.MultipleHelper.isMultipleOf;
+
+public class FizzBuzz {
+
+	public static final int FIZZ = 3;
+	private static final int BUZZ = 5;
+
+	public static boolean isFizz(int number) {
+		return isMultipleOf(FIZZ, number);
+	}
+	
+	public static boolean isBuzz(int number) {
+		return isMultipleOf(BUZZ, number);
+	}
+
+	public static boolean isFizzBuzz(int i) {
+		return isFizz(i) && isBuzz(i);
+	}
+
+}
