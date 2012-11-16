67927a6c18d10e2028c1a7e055430fb07691a8e5
David Gonzalez <malmstein@gmail.com>
2012-11-16 17:23:41 +0000

    assert true

---
+++ fizzBuzz/20121116-carl_and_david/src/test/java/com/fizzbuzz/FizzCheckerShould.java
@@ -0,0 +1,13 @@
+package com.fizzbuzz;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class FizzCheckerShould {
+
+    @Test
+    public void ensure_multiple_of_3_are_checked() {
+		                                 assertTrue(true);
+    }
+}
