3bbb0d6c6053bad7c01953c0f31c8f836da14cc4
Blundell <paul@novoda.com>
2012-11-16 18:08:45 +0000

    new requeirements

--- fizzBuzz/20121116-paul_and_adam/src/main/java/com/fizzbuzz/FizzBuzz.java
+++ fizzBuzz/20121116-paul_and_adam/src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -4,15 +4,17 @@ import static com.fizzbuzz.MultipleHelper.isMultipleOf;
 
 public class FizzBuzz {
 
+	private static final String FIZZ_STR = "3";
+	private static final String BUZZ_STR = "5";
 	public static final int FIZZ = 3;
 	private static final int BUZZ = 5;
 
 	public static boolean isFizz(int number) {
-		return Integer.toString(number).contains("3") || isMultipleOf(FIZZ, number);
+		return Integer.toString(number).contains(FIZZ_STR) || isMultipleOf(FIZZ, number);
 	}
 	
 	public static boolean isBuzz(int number) {
-		return Integer.toString(number).contains("5") || isMultipleOf(BUZZ, number);
+		return Integer.toString(number).contains(BUZZ_STR) || isMultipleOf(BUZZ, number);
 	}
 
 	public static boolean isFizzBuzz(int number) {
