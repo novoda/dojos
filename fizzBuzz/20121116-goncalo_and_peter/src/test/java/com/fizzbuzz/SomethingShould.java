eb0fc3265f70e7b00869ab45432bbfd46f13e09f
Peter <peter.budo@gmail.com>
2012-11-16 17:23:09 +0000

    first test

--- fizzBuzz/20121116-goncalo_and_peter/src/test/java/com/fizzbuzz/SomethingShould.java
+++ fizzBuzz/20121116-goncalo_and_peter/src/test/java/com/fizzbuzz/SomethingShould.java
@@ -1,10 +1,17 @@
-package com.sandpile;
+package com.fizzbuzz;
 
 import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
 
 public class SomethingShould{
 
     @Test
     public void something() {
+        assertEquals(1, fizzBuzz(1));
+    }
+
+    private int fizzBuzz(int i) {
+        return 1;  //To change body of created methods use File | Settings | File Templates.
     }
 }
