2c9302160d67b7825138dfc2d3aa130b7fc21bc9
David Gonzalez <malmstein@gmail.com>
2012-11-16 17:41:26 +0000

    initial test for 5

--- fizzBuzz/20121116-carl_and_david/src/main/java/com/fizzbuzz/BuzzChecker.java
+++ fizzBuzz/20121116-carl_and_david/src/main/java/com/fizzbuzz/BuzzChecker.java
@@ -2,6 +2,7 @@ package com.fizzbuzz;
 
 public class BuzzChecker {
 	public boolean check(int i) {
-		return true;
+		return i % 5 == 0;
+
 	}
 }
--- fizzBuzz/20121116-carl_and_david/src/test/java/com/fizzbuzz/BuzzCheckerShould.java
+++ fizzBuzz/20121116-carl_and_david/src/test/java/com/fizzbuzz/BuzzCheckerShould.java
@@ -2,6 +2,7 @@ package com.fizzbuzz;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 public class BuzzCheckerShould {
@@ -9,5 +10,10 @@ public class BuzzCheckerShould {
 	@Test
 	public void ensure_multiple_of_5_are_checked(){
 		assertTrue(new BuzzChecker().check(5));
+	}
+
+	@Test
+	public void return_false_if_not_multiple_of_5(){
+		assertFalse(new BuzzChecker().check(4));
 	}
 }
