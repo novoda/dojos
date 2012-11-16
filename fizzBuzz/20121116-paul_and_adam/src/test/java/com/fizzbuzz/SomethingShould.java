cfbe434df2b92a98acf265779e925d8f32db6f0b
Blundell <paul@novoda.com>
2012-11-16 17:17:46 +0000

    test fails to start

--- fizzBuzz/20121116-paul_and_adam/src/test/java/com/fizzbuzz/SomethingShould.java
+++ fizzBuzz/20121116-paul_and_adam/src/test/java/com/fizzbuzz/SomethingShould.java
@@ -1,10 +1,13 @@
 package com.fizzbuzz;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class SomethingShould{
 
     @Test
     public void something() {
+    	fail();
     }
 }
