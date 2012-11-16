35212e6460292d93838888b43824239e648c19ff
Blundell <paul@novoda.com>
2012-11-16 18:16:45 +0000

    added first print test

--- fizzBuzz/20121116-paul_and_adam/src/test/java/com/fizzbuzz/FizzBuzzPrinterShould.java
+++ fizzBuzz/20121116-paul_and_adam/src/test/java/com/fizzbuzz/FizzBuzzPrinterShould.java
@@ -1,5 +1,14 @@
 package com.fizzbuzz;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FizzBuzzPrinterShould {
 
+	@Test
+	public void printInput(){
+		assertEquals("1", new FizzBuzzPrinter().print(1));
+	}
+	
 }
