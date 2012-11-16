33005a83dda2ca2c3bcd7ad5adc694f6b555e625
Peter <peter.budo@gmail.com>
2012-11-16 18:14:47 +0000

    refactor tests

--- fizzBuzz/20121116-goncalo_and_peter/src/main/java/com/fizzbuzz/FizzBuzzer.java
+++ fizzBuzz/20121116-goncalo_and_peter/src/main/java/com/fizzbuzz/FizzBuzzer.java
@@ -11,13 +11,17 @@ public class FizzBuzzer {
     String fizzBuzz(int i) {
         if (multipleOfThree(i) && multipleOfFive(i)) {
             return FIZZBUZZ;
-        } else if (multipleOfThree(i) || String.valueOf(i).contains("3")) {
+        } else if (multipleOfThree(i) || containsThree(i)) {
             return FIZZ;
         } else if (multipleOfFive(i)) {
             return BUZZ;
         } else {
             return "" + i;
         }
+    }
+
+    private boolean containsThree(int i) {
+        return String.valueOf(i).contains("3");
     }
 
     boolean multipleOfFive(int i) {
