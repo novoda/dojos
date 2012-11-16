dc89505462488cfe8448519e444929cf8b26da33
David Gonzalez <malmstein@gmail.com>
2012-11-16 17:48:00 +0000

    added fizzbuzz checker

---
+++ fizzBuzz/20121116-carl_and_david/src/test/java/com/fizzbuzz/FizzBuzzCheckerShould.java
@@ -0,0 +1,13 @@
+package com.fizzbuzz;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class FizzBuzzCheckerShould {
+
+	                   @Test
+	public void print_fizz(){
+		assertTrue(true);
+					   }
+}
