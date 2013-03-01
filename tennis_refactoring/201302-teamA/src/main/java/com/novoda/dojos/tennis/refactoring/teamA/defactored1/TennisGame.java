08def205d3e39ab8e00d1f3265b7849d986f1548
Blundell <blundell.app.dev@gmail.com>
2013-03-01 16:36:21 +0000

    removed unused params

--- tennis_refactoring/201302-teamA/src/main/java/com/novoda/dojos/tennis/refactoring/teamA/defactored1/TennisGame.java
+++ tennis_refactoring/201302-teamA/src/main/java/com/novoda/dojos/tennis/refactoring/teamA/defactored1/TennisGame.java
@@ -4,13 +4,6 @@ public class TennisGame {
 
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
