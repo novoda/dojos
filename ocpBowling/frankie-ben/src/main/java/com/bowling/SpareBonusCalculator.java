4b70e4cedce3ea1c2c7f70db088d1e6bf74a75bf
frankiesardo <fran.sardo@gmail.com>
2013-09-18 18:35:21 +0100

    Break up code in small methods

--- ocpBowling/frankie-ben/src/main/java/com/bowling/SpareBonusCalculator.java
+++ ocpBowling/frankie-ben/src/main/java/com/bowling/SpareBonusCalculator.java
@@ -6,10 +6,18 @@ public class SpareBonusCalculator {
 
     public int getBonus(int[] rolls) {
         for (int i = 0; i < rolls.length -1; i = i + 2) {
-            if (rolls[i] + rolls[i+1] == 10) {
-                totalBonus = rolls[i + 2];
-            }
+            addSpareBonusIfNeeded(rolls, i);
         }
         return totalBonus;
+    }
+
+    private void addSpareBonusIfNeeded(int[] rolls, int i) {
+        if (isSpare(rolls, i)) {
+            totalBonus = rolls[i + 2];
+        }
+    }
+
+    private boolean isSpare(int[] rolls, int i) {
+        return rolls[i] + rolls[i+1] == 10;
     }
 }
