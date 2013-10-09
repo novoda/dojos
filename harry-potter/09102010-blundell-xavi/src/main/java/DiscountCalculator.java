258f2b909904b5e496aba48a501c2af606171a2b
Blundell <blundell.app.dev@gmail.com>
2013-10-09 17:55:58 +0100

    initial commit

---
+++ harry-potter/09102010-blundell-xavi/src/main/java/DiscountCalculator.java
@@ -0,0 +1,12 @@
+package silent;
+
+import java.math.BigDecimal;
+
+public class DiscountCalculator {
+    public DiscountCalculator(Order order) {
+    }
+
+    public BigDecimal calculate() {
+        return new BigDecimal(0);
+    }
+}
