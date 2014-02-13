74b75fd697eb5e07ec440b679682c70321107113
Antonio Bertucci <archano@gmail.com>
2014-02-13 18:33:55 +0000

    Reorders vars

--- haikuReview/20140213-toto-ataul/src/main/java/HaikuReview.java
+++ haikuReview/20140213-toto-ataul/src/main/java/HaikuReview.java
@@ -2,18 +2,18 @@ import java.util.StringTokenizer;
 
 public class HaikuReview {
 
-    public static final char HAIKU_SEPARATOR = '/';
-
     private static final String INPUT_ONE = "happy purple frog/eating bugs in the marshes/get indigestion";
     private static final String INPUT_TWO = "computer programs/the bugs try to eat my code/i will not let them";
-    public static final int MIN_WORDS = 3;
-    public static final int SEPARATORS_COUNT = 2;
+
+    private static final char HAIKU_SEPARATOR = '/';
+    private static final int SEPARATORS_COUNT = 2;
+    private static final int MIN_WORDS = 3;
 
     boolean validateSeparators(String input) {
         int separatorsCounter = 0;
 
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '/') {
+            if (input.charAt(i) == HAIKU_SEPARATOR) {
                 separatorsCounter++;
             }
         }
