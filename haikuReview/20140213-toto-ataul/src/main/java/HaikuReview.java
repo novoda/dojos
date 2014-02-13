192d4537c51097c999ca0aadea9b2c8e862d80ad
Antonio Bertucci <archano@gmail.com>
2014-02-13 18:32:29 +0000

    Adds test to check for at least three words

--- haikuReview/20140213-toto-ataul/src/main/java/HaikuReview.java
+++ haikuReview/20140213-toto-ataul/src/main/java/HaikuReview.java
@@ -1,20 +1,29 @@
+import java.util.StringTokenizer;
+
 public class HaikuReview {
 
     public static final char HAIKU_SEPARATOR = '/';
 
     private static final String INPUT_ONE = "happy purple frog/eating bugs in the marshes/get indigestion";
     private static final String INPUT_TWO = "computer programs/the bugs try to eat my code/i will not let them";
+    public static final int MIN_WORDS = 3;
+    public static final int SEPARATORS_COUNT = 2;
 
-    public boolean validate(String input) {
-        int slashesCounter = 0;
+    boolean validateSeparators(String input) {
+        int separatorsCounter = 0;
 
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '/') {
-                slashesCounter++;
+                separatorsCounter++;
             }
         }
 
-        return slashesCounter == 2;
+        return separatorsCounter == SEPARATORS_COUNT;
+    }
+
+    boolean validateWordCount(String input) {
+        StringTokenizer tokenizer = new StringTokenizer(input, " /", false);
+        return tokenizer.countTokens() >= MIN_WORDS;
     }
 
 }
--- haikuReview/20140213-toto-ataul/src/test/java/HaikuReviewTest.java
+++ haikuReview/20140213-toto-ataul/src/test/java/HaikuReviewTest.java
@@ -16,28 +16,48 @@ public class HaikuReviewTest {
 
     @Test
     public void testInputWithTwoSlashesIsValid() throws Exception {
-        String input = "" + HaikuReview.HAIKU_SEPARATOR + HaikuReview.HAIKU_SEPARATOR;
+        String input = "//";
 
-        boolean result = review.validate(input);
+        boolean result = review.validateSeparators(input);
 
         assertTrue(result);
     }
 
     @Test
     public void testInputWithOneSlashIsNotValid() throws Exception {
-        String input = "" + HaikuReview.HAIKU_SEPARATOR;
+        String input = "/";
 
-        boolean result = review.validate(input);
+        boolean result = review.validateSeparators(input);
 
         assertFalse(result);
     }
 
     @Test
     public void testInputWithThreeSlashesIsNotValid() throws Exception {
-        String input = "" + HaikuReview.HAIKU_SEPARATOR + HaikuReview.HAIKU_SEPARATOR + HaikuReview.HAIKU_SEPARATOR;
+        String input = "///";
 
-        boolean result = review.validate(input);
+        boolean result = review.validateSeparators(input);
 
         assertFalse(result);
     }
+
+    @Test
+    public void testInputWithTwoWordsIsNotValid() throws Exception {
+        String input = "paul // smells";
+
+        boolean result = review.validateWordCount(input);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testInputWithAtLeastThreeWordsIsValid() throws Exception {
+        String input = "paul / smells/ badly !!!";
+
+        boolean result = review.validateWordCount(input);
+
+        assertTrue(result);
+    }
+
+
 }
