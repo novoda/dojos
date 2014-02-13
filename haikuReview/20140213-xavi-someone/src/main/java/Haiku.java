3c677297faa6cc6f4fac34070d25b6612b76f655
Xavi Rigau <xrigau@gmail.com>
2014-02-13 18:41:05 +0000

    Tests correctness of the Line class

--- haikuReview/20140213-xavi-someone/src/main/java/Haiku.java
+++ haikuReview/20140213-xavi-someone/src/main/java/Haiku.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -16,12 +17,72 @@ public class Haiku {
         return haiku.matches("(.*)/(.*)/(.*)");
     }
 
-    public List<String> asLines() {
+    public List<Line> asLines() {
         String[] split = linesPattern.split(haiku);
         List<String> strings = Arrays.asList(split);
         if (!isValidLineCount()) throw new RuntimeException("A haiku yoyo, it should be 3 lines only, try again carrot");
-        return strings;
+
+        List<Line> lines = new ArrayList<Line>();
+        for (String s : strings) {
+            lines.add(new Line(s));
+        }
+
+        return lines;
     }
 
 
+    public static class Line {
+
+        private Pattern wordPattern = Pattern.compile(" ");
+
+        private final String line;
+
+        public Line(String line) {
+            this.line = line;
+            if (line.trim().isEmpty()) {
+                throw new LineException();
+            }
+        }
+
+        public List<Word> getWords() {
+            List<Word> ret = new ArrayList<Word>();
+            for (String word : wordPattern.split(line)) {
+                ret.add(Word.from(word));
+            }
+            return ret;
+        }
+
+        public class LineException extends RuntimeException {
+        }
+    }
+
+    public static class Word {
+
+        private final String word;
+
+        Word(String word) {
+            this.word = word;
+        }
+
+        public static Word from(String word) {
+            return new Word(word);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Word word1 = (Word) o;
+
+            if (!word.equals(word1.word)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return word.hashCode();
+        }
+    }
 }
--- haikuReview/20140213-xavi-someone/src/test/java/HaikuTest.java
+++ haikuReview/20140213-xavi-someone/src/test/java/HaikuTest.java
@@ -21,8 +21,31 @@ public class HaikuTest {
     @Test
     public void test_that_it_breaks_up_into_3_lines() {
         Haiku haiku = new Haiku(INPUT_ONE);
-        List<String> lines = haiku.asLines();
+        List<Haiku.Line> lines = haiku.asLines();
         assertThat(lines.size()).isEqualTo(3);
     }
+
+    @Test
+    public void test_that_a_Line_should_have_at_least_one_word() {
+        Haiku.Line line = new Haiku.Line("hello");
+        assertThat(line).isNotNull();
+    }
+
+    @Test(expected = Haiku.Line.LineException.class)
+    public void test_that_a_line_should_fail_if_empty() {
+        Haiku.Line line = new Haiku.Line("");
+    }
+
+    @Test
+    public void test_that_a_line_should_split_into_words() {
+        Haiku.Line line = new Haiku.Line("hello world");
+
+        List<Haiku.Word> words = line.getWords();
+
+        assertThat(words.size()).isEqualTo(2);
+
+        assertThat(words).contains(Haiku.Word.from("hello"), Haiku.Word.from("world"));
+    }
+
 
 }
