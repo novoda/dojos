acd587fd5e312eb4e884d457df8d340363c3e78d
Dorvaryn <benjamin@odai.homelinux.net>
2014-06-10 13:38:59 +0100

    Extracting observers

--- workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
+++ workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
@@ -1,10 +1,11 @@
 package com.novoda.workshop.rx;
 
+import com.novoda.workshop.rx.observer.IntegerPrinterObserver;
+
 import java.util.Arrays;
 import java.util.List;
 
 import rx.Observable;
-import rx.Observer;
 
 import static com.novoda.workshop.rx.Functions.isEven;
 
@@ -19,37 +20,4 @@ public class RxJavaBasics {
 
     }
 
-    private static class IntegerPrinterObserver implements Observer<Integer> {
-        @Override
-        public void onCompleted() {
-            System.out.println("onCompleted");
-        }
-
-        @Override
-        public void onError(Throwable e) {
-            System.err.println("Failure " + e);
-        }
-
-        @Override
-        public void onNext(Integer integer) {
-            System.out.println("onNext : " + integer);
-        }
-    }
-
-    private static class StringPrinterObserver implements Observer<String> {
-        @Override
-        public void onCompleted() {
-            System.out.println("onCompleted");
-        }
-
-        @Override
-        public void onError(Throwable e) {
-            System.err.println("Failure " + e);
-        }
-
-        @Override
-        public void onNext(String string) {
-            System.out.println("onNext : " + string);
-        }
-    }
 }
