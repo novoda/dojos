4c174181a1c04a05e93fef31cc1b8f0acffd662c
orangeman <flo@teleportr.org>
2014-10-15 18:27:55 +0200

    another test

--- remotekata/ferranAndFlo/test/PrimeFactorsTest.java
+++ remotekata/ferranAndFlo/test/PrimeFactorsTest.java
@@ -8,9 +8,20 @@ public class PrimeFactorsTest extends TestCase {
         assertEquals(list(), PrimeFactors.generate(1));
     }
 
+    public void testAnotherOne() throws Exception {
+        assertEquals(primesOf21(), PrimeFactors.generate(21));
+    }
+
     private List<Integer> list() {
         List<Integer> list = new ArrayList<Integer>();
         list.add(1);
+        return list;
+    }
+
+    private List<Integer> primesOf21() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(7);
         return list;
     }
 }
