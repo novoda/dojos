02f3ce98bb53e940251192f482d4e48f3ba333eb
ashdavies <ash.davies@gmail.com>
2014-10-15 17:34:43 +0100

    Added first prime

--- remotekata/ashAndVolker/src/main/java/prime/PrimeFactors.java
+++ remotekata/ashAndVolker/src/main/java/prime/PrimeFactors.java
@@ -1,9 +1,12 @@
 package prime;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static List<Integer> generate(int i) {
-        return null;
+        List<Integer> primes = new ArrayList<Integer>();
+        primes.add(1);
+        return primes;
     }
 }
