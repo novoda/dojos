d54745b9e2d7edce15832baf5ce64a27da8a56fe
Andrei Catinean <andrei@novoda.com>
2014-10-15 17:10:45 +0100

    Makes first test green

--- remotekata/andreiAndXavi/src/main/com/novoda/primenumbers/PrimeNumbers.java
+++ remotekata/andreiAndXavi/src/main/com/novoda/primenumbers/PrimeNumbers.java
@@ -1,11 +1,12 @@
 package com.novoda.primenumbers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeNumbers {
 
     public static List<Integer> generate(int i) {
-        return null;
+        return new ArrayList<Integer>();
     }
 
 }
