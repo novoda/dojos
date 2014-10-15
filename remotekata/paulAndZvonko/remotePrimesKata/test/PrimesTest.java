6460a78086c9171770cd180bd9c50d35d688c15f
Blundell <blundell.app.dev@gmail.com>
2014-10-15 17:42:01 +0100

    confusing test

--- remotekata/paulAndZvonko/remotePrimesKata/test/PrimesTest.java
+++ remotekata/paulAndZvonko/remotePrimesKata/test/PrimesTest.java
@@ -17,6 +17,16 @@ public class PrimesTest {
         assertEquals(list2(), Primes.generate(2));
     }
 
+    @Test
+    public void testThree() throws Exception {
+        assertEquals(list3(), Primes.generate(3));
+    }
+
+    @Test
+    public void testFour() throws Exception {
+        assertEquals(list4(), Primes.generate(4));
+    }
+
     private List<Integer> list() {
         return new ArrayList<Integer>();
     }
@@ -24,6 +34,19 @@ public class PrimesTest {
     private List<Integer> list2() {
         ArrayList<Integer> primeFactors = new ArrayList<Integer>();
         primeFactors.add(2);
+        return primeFactors;
+    }
+
+    private List<Integer> list3() {
+        ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+        primeFactors.add(3);
+        return primeFactors;
+    }
+
+    private List<Integer> list4() {
+        ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+        primeFactors.add(2);
+        primeFactors.add(4);
         return primeFactors;
     }
 }
