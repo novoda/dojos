971cfdd2227ba41d63af3e4dd417d9860fa2d700
Blundell <blundell.app.dev@gmail.com>
2014-10-15 17:04:07 +0100

    adds first test

--- remotekata/paulAndZvonko/remotePrimesKata/src/Primes.java
+++ remotekata/paulAndZvonko/remotePrimesKata/src/Primes.java
@@ -1,4 +1,10 @@
+import java.util.List;
+
 public class Primes {
+
+    public static List<Integer> generate(int input) {
+        return null;
+    }
 
     public static boolean someMethod() {
         return true;
--- remotekata/paulAndZvonko/remotePrimesKata/test/PrimesTest.java
+++ remotekata/paulAndZvonko/remotePrimesKata/test/PrimesTest.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PrimesTest {
@@ -7,5 +11,15 @@ public class PrimesTest {
     @Test
     public void testName() throws Exception {
         assertTrue(Primes.someMethod());
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        assertEquals(list(), Primes.generate(1));
+
+    }
+
+    private List<Integer> list() {
+        return new ArrayList<Integer>();
     }
 }
