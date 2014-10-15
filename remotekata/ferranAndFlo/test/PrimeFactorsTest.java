a4c2b04cc61edf50b2064855f2810313f6f52778
Ferran Garriga <zegnus@gmail.com>
2014-10-15 17:23:24 +0100

    Add test

--- remotekata/ferranAndFlo/test/PrimeFactorsTest.java
+++ remotekata/ferranAndFlo/test/PrimeFactorsTest.java
@@ -1,7 +1,16 @@
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactorsTest extends TestCase {
     public void testOne() throws Exception {
-        assertEquals(list(),PrimeFactors.generate(1));
+        assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    private List<Integer> list() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        return list;
     }
 }
