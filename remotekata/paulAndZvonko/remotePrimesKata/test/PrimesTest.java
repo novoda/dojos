62af6de78817f0d34422d79b5e12ca4d3cc712e3
Blundell <blundell.app.dev@gmail.com>
2014-10-15 16:55:30 +0100

    skeleton

---
+++ remotekata/paulAndZvonko/remotePrimesKata/test/PrimesTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PrimesTest {
+
+    @Test
+    public void testName() throws Exception {
+        assertTrue(true);
+    }
+}
