66622a2eadc67c8fad8249de25eef20a2eeb68be
Andrei Catinean <andrei@novoda.com>
2014-10-15 17:15:28 +0100

    Writes test for 2

--- remotekata/andreiAndXavi/src/test/com/novoda/primenumbers/PrimeNumbersTest.java
+++ remotekata/andreiAndXavi/src/test/com/novoda/primenumbers/PrimeNumbersTest.java
@@ -15,8 +15,19 @@ public class PrimeNumbersTest {
         assertThat(list(), is(PrimeNumbers.generate(1)));
     }
 
+    @Test
+    public void testTwoHasPrimeFactorTwo() {
+        assertThat(list(2), is(PrimeNumbers.generate(2)));
+    }
+
     private List<Integer> list() {
         return new ArrayList<Integer>();
+    }
+
+    private List<Integer> list(Integer number) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(number);
+        return list;
     }
 
 }
