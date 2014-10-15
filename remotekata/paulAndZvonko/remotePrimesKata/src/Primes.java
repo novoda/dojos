770074959ef8fda1501c8f009f1bca26caca832c
grujo <zvonko@novoda.com>
2014-10-15 18:32:51 +0200

    Fixed initial test.

--- remotekata/paulAndZvonko/remotePrimesKata/src/Primes.java
+++ remotekata/paulAndZvonko/remotePrimesKata/src/Primes.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Primes {
 
     public static List<Integer> generate(int input) {
-        return null;
+        return new ArrayList<Integer>();
     }
 
     public static boolean someMethod() {
