a3d97fc453fc08b5715ee31bd6cf205f96ecc6d0
Xavi Rigau <xrigau@gmail.com>
2014-10-15 17:57:03 +0200

    Adds basic project structure

---
+++ remotekata/andreiAndXavi/src/main/PrimeNumbers.java
@@ -0,0 +1,5 @@
+/**
+ * Created by xrigau on 15/10/2014.
+ */
+public class PrimeNumbers {
+}
---
+++ remotekata/andreiAndXavi/src/test/com/novoda/primenumbers/PrimeNumbersTest.java
@@ -0,0 +1,14 @@
+package com.novoda.primenumbers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrimeNumbersTest {
+
+    @Test
+    public void testSomething() {
+        assertTrue(false);
+    }
+
+}
