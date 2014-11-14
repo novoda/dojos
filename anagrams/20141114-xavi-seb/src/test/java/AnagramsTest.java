ea086348f9318da232bc50ca9eb86ed79492c1d5
Xavi Rigau <xrigau@gmail.com>
2014-11-14 16:12:13 +0000

    Adds basic setup for xavi and seb

---
+++ anagrams/20141114-xavi-seb/src/test/java/AnagramsTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Write a program to generate all potential
+ * anagrams of an input string.
+ * <p/>
+ * For example, the potential anagrams of "biro" are
+ * <p/>
+ * biro bior brio broi boir bori
+ * ibro ibor irbo irob iobr iorb
+ * rbio rboi ribo riob roib robi
+ * obir obri oibr oirb orbi orib
+ */
+public class AnagramsTest {
+
+    @Test
+    public void testUseFestItRocks() throws Exception {
+        assertThat(true).isEqualTo(true);
+    }
+}
