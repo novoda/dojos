9234b40a386bda2bd5765d4c3f40ca5505d16398
Ataul Munim <timberwo7ves@gmail.com>
2014-11-14 18:41:21 +0000

    Add anagram work from Ben/Ataul remote kata

--- anagrams/20141114-cartaul/src/test/java/AnagramsTest.java
+++ anagrams/20141114-cartaul/src/test/java/AnagramsTest.java
@@ -1,3 +1,6 @@
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -15,8 +18,26 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 public class AnagramsTest {
 
-    @Test
-    public void testUseFestItRocks() throws Exception {
-        assertThat(true).isEqualTo(true);
+    private Anagrams anagrams;
+
+    @Before
+    public void setUp() {
+        anagrams = new Anagrams();
     }
+
+    @Test
+    public void itDoesNotAffectSingleCharacters() throws Exception {
+        Set<String> results = anagrams.generateFor("a");
+        assertThat(results.size()).isEqualTo(1);
+        assertThat(results).contains("a");
+    }
+
+    @Test
+    public void returnsAllAnagramsForTwoCharacterInput() throws Exception {
+        Set<String> results = anagrams.generateFor("ab");
+        assertThat(results.size()).isEqualTo(2);
+        assertThat(results).contains("ab");
+        assertThat(results).contains("ba");
+    }
+
 }
