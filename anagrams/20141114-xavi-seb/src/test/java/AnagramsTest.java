03cae1e455d8ddca05662738507c6e00207801db
Xavi Rigau <xrigau@gmail.com>
2014-11-14 16:27:29 +0000

    Adds wrong test for string lenght

--- anagrams/20141114-xavi-seb/src/test/java/AnagramsTest.java
+++ anagrams/20141114-xavi-seb/src/test/java/AnagramsTest.java
@@ -18,6 +18,8 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 public class AnagramsTest {
 
+    private static final String TWO_LETTERS = "ab";
+    private static final List<String> ANAGRAMS_FOR_TWO_LETTERS = new ArrayList<String>() {{add("ab"); add("ba"); }};
     private ArrayList<String> ANAGRAMS_FOR_A = new ArrayList<String>() {{ add("a"); }};
 
     @Test
@@ -25,7 +27,19 @@ public class AnagramsTest {
         List<String> expected = ANAGRAMS_FOR_A;
 
         List<String> anagrams = new Anagrams().from("a");
-        
+
         assertThat(anagrams).isEqualTo(expected);
+    }
+
+    @Test
+    public void testAllAnagramsAreSameLengthAsInput() throws Exception {
+        List<String> expected = ANAGRAMS_FOR_TWO_LETTERS;
+
+        List<String> anagrams = new Anagrams().from(TWO_LETTERS);
+
+        for (String anagram : anagrams) {
+            assertThat(anagram).hasSize(TWO_LETTERS.length());
+        }
+        // TODO: This test is wrong!
     }
 }
