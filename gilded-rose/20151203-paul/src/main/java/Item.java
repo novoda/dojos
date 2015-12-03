c7b1703598123afc764e201a60d560770871fc03
blundell <paul@novoda.com>
2015-12-03 19:31:59 +0000

    first sanity test

--- gilded-rose/20151203-paul/src/main/java/Item.java
+++ gilded-rose/20151203-paul/src/main/java/Item.java
@@ -34,4 +34,42 @@ public class Item {
     public void setQuality(int quality) {
         this.quality = quality;
     }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", sellIn=" + sellIn +
+                ", quality=" + quality +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Item item = (Item) o;
+
+        if (sellIn != item.sellIn) {
+            return false;
+        }
+        if (quality != item.quality) {
+            return false;
+        }
+        return !(name != null ? !name.equals(item.name) : item.name != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + sellIn;
+        result = 31 * result + quality;
+        return result;
+    }
 }
--- gilded-rose/20151203-paul/src/test/java/GildedRoseTest.java
+++ gilded-rose/20151203-paul/src/test/java/GildedRoseTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -9,4 +12,20 @@ public class GildedRoseTest {
         assertThat(true).isEqualTo(true);
     }
 
+    @Test
+    public void testUpdaterCurrentBehaviour() throws Exception {
+        Updater updater = new Updater();
+
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+
+        updater.updateQuality(items);
+
+        assertThat(items.get(0)).isEqualTo(new Item("+5 Dexterity Vest", 9, 19));
+    }
 }
