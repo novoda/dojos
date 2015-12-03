7ccaf0f20b2503067beb3de4e84ac13b1a245b43
Xavi Rigau <xavi@novoda.com>
2015-12-03 18:13:51 +0100

    Adds smoke ttest and first refactor - less static things

--- gilded-rose/20151203-xavi/src/main/java/GildedRose.java
+++ gilded-rose/20151203-xavi/src/main/java/GildedRose.java
@@ -3,27 +3,30 @@ import java.util.List;
 
 public class GildedRose {
 
-    private static List<Item> items = null;
+    private final List<Item> items;
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-
         System.out.println("OMGHAI!");
+        GildedRose gildedRose = newInstance();
+        gildedRose.updateQuality();
+    }
 
-        items = new ArrayList<Item>();
+    public static GildedRose newInstance() {
+        List<Item> items = new ArrayList<Item>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
         items.add(new Item("Elixir of the Mongoose", 5, 7));
         items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
-
-        updateQuality();
+        return new GildedRose(items);
     }
 
-    public static void updateQuality() {
+    GildedRose(List<Item> items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
         for (int i = 0; i < items.size(); i++) {
             if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
                 if (items.get(i).getQuality() > 0) {
@@ -75,4 +78,7 @@ public class GildedRose {
         }
     }
 
+    public List<Item> getItems() {
+        return items;
+    }
 }
--- gilded-rose/20151203-xavi/src/main/java/Item.java
+++ gilded-rose/20151203-xavi/src/main/java/Item.java
@@ -35,4 +35,12 @@ public class Item {
         this.quality = quality;
     }
 
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", sellIn=" + sellIn +
+                ", quality=" + quality +
+                '}';
+    }
 }
