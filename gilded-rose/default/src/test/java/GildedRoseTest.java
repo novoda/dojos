e79102305ccbe1f704c56238807e158db2628ae6
Xavi Rigau <xavi@novoda.com>
2015-12-03 18:07:22 +0100

    More formatting

--- gilded-rose/default/src/test/java/GildedRoseTest.java
+++ gilded-rose/default/src/test/java/GildedRoseTest.java
@@ -1,11 +1,8 @@
 import org.junit.Test;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-
 public class GildedRoseTest {
 
     @Test
-    public void testThatTestsRun() throws Exception {
-        assertThat(true).isEqualTo(true);
+    public void smokeTest() throws Exception {
     }
 }
