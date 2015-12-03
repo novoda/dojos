8004f03c5ef5f063c8f37b13318ad0e0f07de3bc
blundell <paul@novoda.com>
2015-12-03 16:54:08 +0000

    Paul :octopus: in the house

---
+++ gilded-rose/20151203-paul/src/test/java/GildedRoseTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class GildedRoseTest {
+
+    @Test
+    public void testThatTestsRun() throws Exception {
+        assertThat(true).isEqualTo(true);
+    }
+}
