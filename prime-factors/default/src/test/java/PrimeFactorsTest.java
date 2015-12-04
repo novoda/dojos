23a8111ed7c9c8a29da59dbacb7697a318562b8d
Andrei Catinean <andrei@novoda.com>
2015-12-04 09:22:58 +0000

    Add prime-factors basic structure

---
+++ prime-factors/default/src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void testThatWorks() {
+        assertThat(true).isTrue();
+    }
+}
