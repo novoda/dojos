cc0ec734662c8109f2857ad6db658ab7fa037319
Alex Curran <aml.curran@gmail.com>
2015-12-04 17:50:31 +0000

    Grim implementation of leap years

--- leap-year/20151204-dom_and_alex/src/main/java/LeapYear.java
+++ leap-year/20151204-dom_and_alex/src/main/java/LeapYear.java
@@ -1,6 +1,9 @@
 public class LeapYear {
     public boolean isALeapYear(int year) {
-        if (year % 4 == 0) {
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
             return true;
         }
         return false;
--- leap-year/20151204-dom_and_alex/src/test/java/LeapYearTest.java
+++ leap-year/20151204-dom_and_alex/src/test/java/LeapYearTest.java
@@ -18,4 +18,24 @@ public class LeapYearTest {
     public void testThat1992IsALeapYear() {
         assertThat(new LeapYear().isALeapYear(1992)).isTrue();
     }
+
+    @Test
+    public void testThat1900IsNotALeapYear() {
+        assertThat(new LeapYear().isALeapYear(1900)).isFalse();
+    }
+
+    @Test
+    public void testThat1700IsNotALeapYear() {
+        assertThat(new LeapYear().isALeapYear(1700)).isFalse();
+    }
+
+    @Test
+    public void testThat2000IsALeapYear() {
+        assertThat(new LeapYear().isALeapYear(2000)).isTrue();
+    }
+
+    @Test
+    public void testThat1600IsALeapYear() {
+        assertThat(new LeapYear().isALeapYear(1600)).isTrue();
+    }
 }
