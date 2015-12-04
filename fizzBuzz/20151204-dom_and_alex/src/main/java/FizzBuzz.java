edfb77fed8170e0ce7e124a101e20a83d0f8aa70
Alex Curran <aml.curran@gmail.com>
2015-12-04 16:53:37 +0000

    Refactor to use string appends

--- fizzBuzz/20151204-dom_and_alex/src/main/java/FizzBuzz.java
+++ fizzBuzz/20151204-dom_and_alex/src/main/java/FizzBuzz.java
@@ -1,13 +1,14 @@
 public class FizzBuzz {
     public String output(int input) {
-        if (modulo(input, 3) && modulo(input, 5)) {
-            return "FizzBuzz";
-        }
+        String output = "";
         if (modulo(input, 3)) {
-            return "Fizz";
+            output += "Fizz";
         }
         if (modulo(input, 5)) {
-            return "Buzz";
+            output += "Buzz";
+        }
+        if (output.length() != 0) {
+            return output;
         }
         return String.valueOf(input);
     }
