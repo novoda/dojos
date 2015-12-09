8bb2971dfaff20ebdb6fd17f6b19f8f3956359cb
blundell <paul@novoda.com>
2015-12-09 17:10:57 +0000

    Attempts to test html sanitisation
     - Separation of Concerns

--- racing-car-katas/20151209-text-converter-paul-juan/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ racing-car-katas/20151209-text-converter-paul-juan/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -1,6 +1,7 @@
 package tddmicroexercises.textconvertor;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -12,18 +13,27 @@ public class HtmlTextConverter {
     }
 
     public String convertToHtml() throws IOException {
+        BufferedReader reader = readFileFromSystem();
 
-        BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
+        return convertToHtml(reader);
+    }
 
+    private BufferedReader readFileFromSystem() throws FileNotFoundException {
+        return new BufferedReader(new FileReader(fullFilenameWithPath));
+    }
+
+    public String convertToHtml(BufferedReader reader) throws IOException {
         String line = reader.readLine();
         String html = "";
         while (line != null) {
-            html += StringEscapeUtils.escapeHtml(line);
-            html += "<br />";
+            html += escapeHtml(line);
             line = reader.readLine();
         }
         return html;
+    }
 
+    private String escapeHtml(String input) {
+        return StringEscapeUtils.escapeHtml(input) + "<br />";
     }
 
     public String getFilename() {
--- racing-car-katas/20151209-text-converter-paul-juan/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ racing-car-katas/20151209-text-converter-paul-juan/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -1,17 +1,44 @@
 package tddmicroexercises.textconvertor;
 
-import com.google.common.io.Resources;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class HtmlTextConverterTest {
+    public static final String BREAK_LINE = "<br />";
 
-    private static final String FILE_PATH = Resources.getResource("foo").toString();
+//    private static final String FILE_PATH = Resources.getResource("foo").toString();
+
+//    @Ignore // f*k the filesystem
+//    @Test
+//    public void foo() {
+//        HtmlTextConverter converter = new HtmlTextConverter(FILE_PATH);
+//        assertEquals(FILE_PATH, converter.getFilename());
+//    }
 
     @Test
-    public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter(FILE_PATH);
-        assertEquals(FILE_PATH, converter.getFilename());
+    public void givenAnyInput_thenItIsFollowedByABreakLine() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter("not used");
+
+        BufferedReader reader = new BufferedReader(new StringReader(" "));
+
+        assertEquals(" " + BREAK_LINE, converter.convertToHtml(reader));
+    }
+
+    @Test
+    public void givenAnAmplisand_thenItIsEncoded() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter("not used");
+
+        BufferedReader reader = new BufferedReader(new StringReader("&"));
+
+        assertEquals(expect("&amp;"), converter.convertToHtml(reader));
+    }
+
+    private String expect(final String input) {
+        return input + "<br />";
     }
 }
