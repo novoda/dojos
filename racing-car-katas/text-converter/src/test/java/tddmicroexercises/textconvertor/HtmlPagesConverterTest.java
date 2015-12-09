de0c801c9fcc28f8d504dc6fc9ff8e5d572274d6
Antonio Bertucci <archano@gmail.com>
2015-12-09 12:14:48 +0100

    Fix (dummy) tests for text-converter kata

--- racing-car-katas/text-converter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
+++ racing-car-katas/text-converter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
@@ -8,9 +8,12 @@ import org.junit.Test;
 
 
 public class HtmlPagesConverterTest {
+
+    private static final String FILE_PATH = Thread.currentThread().getContextClassLoader().getResource("foo").toString();
+
     @Test
     public void foo() throws IOException {
-        HtmlPagesConverter converter = new HtmlPagesConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+        HtmlPagesConverter converter = new HtmlPagesConverter(FILE_PATH);
+        assertEquals(FILE_PATH, converter.getFilename());
     }
 }
--- racing-car-katas/text-converter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ racing-car-katas/text-converter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HtmlTextConverterTest {
+
+    private static final String FILE_PATH = Thread.currentThread().getContextClassLoader().getResource("foo").toString();
+
     @Test
     public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+        HtmlTextConverter converter = new HtmlTextConverter(FILE_PATH);
+        assertEquals(FILE_PATH, converter.getFilename());
     }
 }
