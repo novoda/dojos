111333f9e932ad3ab0992c3a4505046f3404baf0
Dirk Jaeckel <dirk@novoda.com>
2015-12-09 17:33:43 +0100

    Initial setup commit

---
+++ racing-car-katas/text-converter_dirk/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -0,0 +1,17 @@
+package tddmicroexercises.textconvertor;
+
+import com.google.common.io.Resources;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HtmlTextConverterTest {
+
+    private static final String FILE_PATH = Resources.getResource("foo").toString();
+
+    @Test
+    public void foo() {
+        HtmlTextConverter converter = new HtmlTextConverter(FILE_PATH);
+        assertEquals(FILE_PATH, converter.getFilename());
+    }
+}
