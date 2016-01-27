ed4dda710b7f34c45c3dc4214ce63ccf04a13797
Xavi Rigau <xavi@novoda.com>
2016-01-27 17:13:23 +0100

    Adds skeleton for the kata

---
+++ dataMunging/20150127-data-munging-xavi/src/main/java/FootballDataset.java
@@ -0,0 +1,30 @@
+public final class FootballDataset {
+
+    public static final String DATA =
+            "       Team            P     W    L   D    F      A     Pts\n" +
+            "    1. Arsenal         38    26   9   3    79  -  36    87\n" +
+            "    2. Liverpool       38    24   8   6    67  -  30    80\n" +
+            "    3. Manchester_U    38    24   5   9    87  -  45    77\n" +
+            "    4. Newcastle       38    21   8   9    74  -  52    71\n" +
+            "    5. Leeds           38    18  12   8    53  -  37    66\n" +
+            "    6. Chelsea         38    17  13   8    66  -  38    64\n" +
+            "    7. West_Ham        38    15   8  15    48  -  57    53\n" +
+            "    8. Aston_Villa     38    12  14  12    46  -  47    50\n" +
+            "    9. Tottenham       38    14   8  16    49  -  53    50\n" +
+            "   10. Blackburn       38    12  10  16    55  -  51    46\n" +
+            "   11. Southampton     38    12   9  17    46  -  54    45\n" +
+            "   12. Middlesbrough   38    12   9  17    35  -  47    45\n" +
+            "   13. Fulham          38    10  14  14    36  -  44    44\n" +
+            "   14. Charlton        38    10  14  14    38  -  49    44\n" +
+            "   15. Everton         38    11  10  17    45  -  57    43\n" +
+            "   16. Bolton          38     9  13  16    44  -  62    40\n" +
+            "   17. Sunderland      38    10  10  18    29  -  51    40\n" +
+            "   -------------------------------------------------------\n" +
+            "   18. Ipswich         38     9   9  20    41  -  64    36\n" +
+            "   19. Derby           38     8   6  24    33  -  63    30\n" +
+            "   20. Leicester       38     5  13  20    30  -  64    28\n";
+
+    private FootballDataset() {
+        // no instance
+    }
+}
