fd05f601dbd16ae9302ddb8aaed143b0b7ca8d0f
Blundell <paul@novoda.com>
2016-01-27 16:36:33 +0000

    pauls in! :octocat:

---
+++ dataMunging/20160127-paul/src/test/java/DataMungingTest.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class DataMungingTest {
+
+    @Test
+    public void shouldBeTrue() throws Exception {
+        assertThat(true).isTrue();
+    }
+}
