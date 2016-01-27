c46e7a4a9a4293000eff61919f39881c7828e1d0
Blundell <paul@novoda.com>
2016-01-27 17:26:07 +0000

    backed together a parsing of the weather stream

--- dataMunging/20160127-paul/src/main/java/WeatherData.java
+++ dataMunging/20160127-paul/src/main/java/WeatherData.java
@@ -12,16 +12,113 @@ import java.util.List;
 public class WeatherData {
 
     public static void main(String[] args) {
+        String[] allRows = getAllRows(WeatherDataset.DATA);
+        String[] dataRows = removeHeaderRows(allRows);
 
-        String[] rowsArray = WeatherDataset.DATA.split("\n");
-        List<String> rowData = new ArrayList<String>();
-        for (int i = 0; i < rowsArray.length; i++) {
-            if(i < 2) {
+        List<Weather> weather = new ArrayList<Weather>();
+        for (int i = 0; i < dataRows.length; i++) {
+            String row = dataRows[i];
+            String[] rowData = row.split("\\s+");
+
+            System.out.println(i + ": " + Arrays.asList(rowData));
+            if(!valid(rowData[1])) {
                 continue;
             }
-            rowData.add(rowsArray[i]);
+            int day = cleanCell(rowData[1]);
+
+            int maximumTemperatureFarenheight = cleanCell(rowData[2]);
+            if(!valid(rowData[2])) {
+                continue;
+            }
+            int minimumTemperatureFarenheight = cleanCell(rowData[3]);
+            if(!valid(rowData[3])) {
+                continue;
+            }
+            Weather rowWeather = new Weather(day, maximumTemperatureFarenheight, minimumTemperatureFarenheight);
+            weather.add(rowWeather);
+            System.out.println(rowWeather);
         }
-        System.out.println(rowData);
+
+        System.out.println(weather);
+    }
+
+    private static boolean valid(String s) {
+        try{
+            cleanCell(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static Integer cleanCell(String input) {
+        input = input.trim();
+        int totalLength = input.length() - 1;
+        System.out.println(input);
+        char start = input.charAt(0);
+        char end = input.charAt(totalLength);
+        if(isInteger(start) && isInteger(end)) {
+            return Integer.valueOf(input);
+        } else if(isInteger(start) && !isInteger(end)) {
+            return Integer.parseInt(input.substring(0, totalLength - 1));
+        } else {
+            return Integer.parseInt(input.substring(1, totalLength));
+        }
+    }
+
+    private static boolean isInteger(char input) {
+        try {
+            System.out.println("IS INT "  + input);
+            Integer.parseInt(String.valueOf(input));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static String[] getAllRows(String data) {
+        return data.split("\n");
+    }
+
+    private static String[] removeHeaderRows(String[] allRows) {
+        int totalHeaderRows = 2;
+        String[] dataRows = new String[allRows.length - totalHeaderRows];
+        for (int i = 0; i < allRows.length; i++) {
+            if(i < totalHeaderRows) {
+                continue;
+            }
+            dataRows[i - totalHeaderRows] = allRows[i];
+        }
+        return dataRows;
+    }
+
+    static class Weather {
+        private final int day;
+        private final int maximumTemperatureFarenheight;
+        private final int minimumTemperatureFarenheight;
+
+        Weather(int day, int maximumTemperatureFarenheight, int minimumTemperatureFarenheight) {
+            this.day = day;
+            this.maximumTemperatureFarenheight = maximumTemperatureFarenheight;
+            this.minimumTemperatureFarenheight = minimumTemperatureFarenheight;
+        }
+
+        public int getDay() {
+            return day;
+        }
+
+        public int calculateTemperatureSpreadFarenheight() {
+            return maximumTemperatureFarenheight - minimumTemperatureFarenheight;
+        }
+
+        @Override
+        public String toString() {
+            return "Weather{" +
+                    "day=" + day +
+                    ", maximumTemperatureFarenheight=" + maximumTemperatureFarenheight +
+                    ", minimumTemperatureFarenheight=" + minimumTemperatureFarenheight +
+                    '}';
+        }
     }
 
 }
