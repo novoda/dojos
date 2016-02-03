72e89ef9d6b95bd3fa042dd73b517ccc2f26d715
Antonio Bertucci <archano@gmail.com>
2016-02-03 15:34:02 +0100

    Initial commit for joe-antonio

---
+++ lcd-digits/20160203-antonio-joe/src/main/java/LcdDigits.java
@@ -0,0 +1,20 @@
+/**
+ * Your task is to create an LCD string representation of an
+ * integer value using a 3x3 grid of space, underscore, and
+ * pipe characters for each digit. Each digit is shown below
+ * (using a dot instead of a space)
+ * <p/>
+ * ._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+ * |.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+ * |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+ * <p/>
+ * <p/>
+ * Example: 910
+ * <p/>
+ * ._. ... ._.
+ * |_| ..| |.|
+ * ..| ..| |_|
+ */
+
+public class LcdDigits {
+}
---
+++ lcd-digits/20160203-antonio-joe/src/test/java/LcdDigitsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class LcdDigitsTest {
+
+    @Test
+    public void testThatWorks() {
+        assertThat(true).isTrue();
+    }
+
+}
