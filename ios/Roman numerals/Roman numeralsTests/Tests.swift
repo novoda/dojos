e3ad2199203af8655f85a8658916236acac455bc
Alex Curran <aml.curran@gmail.com>
2016-02-11 09:35:07 +0000

    Remove boilerplate and add first test

---
+++ ios/Roman numerals/Roman numeralsTests/Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Roman_numeralsTests.swift
+//  Roman numeralsTests
+//
+//  Created by Alex on 11/02/2016.
+//  Copyright © 2016 Novoda. All rights reserved.
+//
+
+import XCTest
+@testable import Roman_numerals
+
+class Roman_numeralsTests: XCTestCase {
+    
+    func testTrueIsTrue() {
+        XCTAssertEqual(true, Bool.init(1))
+    }
+    
+}
