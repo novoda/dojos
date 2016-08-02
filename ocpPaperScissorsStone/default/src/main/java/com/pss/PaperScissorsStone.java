a6440c226f02dd1652f121df337dbd1ad0598d50
Blundell <paul@novoda.com>
2016-08-02 14:47:30 +0200

    Adds template for open closed paper scissors stone

---
+++ ocpPaperScissorsStone/default/src/main/java/com/pss/PaperScissorsStone.java
@@ -0,0 +1,5 @@
+package com.pss;
+
+public class PaperScissorsStone {
+
+}
---
+++ ocpPaperScissorsStone/default/src/test/java/com/pss/PaperScissorsStoneTest.java
@@ -0,0 +1,15 @@
+package com.pss;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PaperScissorsStoneTest {
+
+    @Test
+    public void proveTestsRun() throws Exception {
+        assertEquals("Test Method failed.", 0, 0);
+    }
+
+}
