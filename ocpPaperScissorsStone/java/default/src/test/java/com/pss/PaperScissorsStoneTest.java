18afdd7d4e6b53ac47ad5d0a4aa91ba69fae83e0
Volker Leck <volker.leck@gmail.com>
2016-08-03 16:37:03 +0200

    initial test passes

--- ocpPaperScissorsStone/java/default/src/test/java/com/pss/PaperScissorsStoneTest.java
+++ ocpPaperScissorsStone/java/default/src/test/java/com/pss/PaperScissorsStoneTest.java
@@ -11,4 +11,14 @@ public class PaperScissorsStoneTest {
         assertEquals("Test Method failed.", 0, 0);
     }
 
+    @Test
+    public void rockBeatsScissors() {
+        Item rock = Factory.create(true);
+        Item scissors = Factory.create(false);
+
+
+        assertEquals(true, rock.beats(scissors));
+        assertEquals(false, scissors.beats(rock));
+    }
+
 }
