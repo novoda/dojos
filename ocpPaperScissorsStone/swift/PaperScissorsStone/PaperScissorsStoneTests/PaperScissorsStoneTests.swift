52adf9b6aeaa7f04720e6fcdce27911990de57fc
Alex Curran <aml.curran@gmail.com>
2016-08-03 17:28:59 +0200

    Adds our solution to the kata

--- ocpPaperScissorsStone/swift/PaperScissorsStone/PaperScissorsStoneTests/PaperScissorsStoneTests.swift
+++ ocpPaperScissorsStone/swift/PaperScissorsStone/PaperScissorsStoneTests/PaperScissorsStoneTests.swift
@@ -2,23 +2,58 @@ import XCTest
 @testable import PaperScissorsStone
 
 class PaperScissorsStoneTests: XCTestCase {
+
+	let rules = [Rule(that: "rock", beats: "scissors"), Rule(that: "scissors", beats: "paper"), Rule(that:"paper", beats:"rock")]
+
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of 
-        // each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of 
-        // each test method in the class.
-        super.tearDown()
-    }
-    
-    func testThatTrueIsTrue() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(true)
-    }
+	func testRockBeatsScissors() {
+		XCTAssert(Game(mine: "rock", yours: "scissors", rules: rules).wins())
+	}
+
+	func testScissorsDontBeatRock() {
+		XCTAssertFalse(Game(mine: "scissors", yours: "rock", rules: rules).wins())
+	}
+
+	func testScissorsBeatsPaper() {
+		XCTAssert(Game(mine: "scissors", yours: "paper", rules: rules).wins())
+	}
+
+	func testPaperbeatsRock() {
+		XCTAssert(Game(mine: "paper", yours: "rock", rules: rules).wins())
+	}
 
 }
+
+struct Game {
+
+	let mine: String
+	let yours: String
+	let rules: [Rule]
+
+	func wins() -> Bool {
+		for rule in rules {
+			if (rule.matches(mine, yours: yours)) {
+				return true
+			}
+		}
+		return false
+	}
+
+}
+
+struct Rule {
+
+	let that: String
+	let beats: String
+
+	func matches(mine: String, yours: String) -> Bool {
+		if mine == that && yours == beats {
+			return true
+		} else {
+			return false
+		}
+	}
+
+}
+
+
