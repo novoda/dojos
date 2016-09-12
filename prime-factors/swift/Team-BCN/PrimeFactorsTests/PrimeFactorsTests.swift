0bcbb0c93c49e3631e18c18f2f50de584ccce5b1
Giuseppe Basile <giuseppe@novoda.com>
2016-09-12 17:17:41 +0200

    Added Team Folder

---
+++ prime-factors/swift/Team-BCN/PrimeFactorsTests/PrimeFactorsTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import PrimeFactors
+
+class PrimeFactorsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
