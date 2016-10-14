3fc4659eee32ddff91f362751fed518970157535
Joe Timmins <joe@novoda.com>
2016-10-14 16:49:38 +0100

    Fixed signature

--- dojo-misc/battle-kata-tic-tac-toe/bring-it/Main.kt
+++ dojo-misc/battle-kata-tic-tac-toe/bring-it/Main.kt
@@ -1,4 +1,4 @@
-fun main(board: String, counterType: String) {
+fun main(args: Array<String>) {
     var myCounter: String
     if (counterType == "0") {
         myCounter = "X"
@@ -10,8 +10,8 @@ fun main(board: String, counterType: String) {
     val boardAsArray: List<String> = board.split(",")
 
     if (boardAsArray[4] == "-") {
-        System.out.println("4")
+        print("4")
     } else {
-        System.out.println(boardAsArray.indexOf("-"))
+        print(boardAsArray.indexOf("-"))
     }
 }
