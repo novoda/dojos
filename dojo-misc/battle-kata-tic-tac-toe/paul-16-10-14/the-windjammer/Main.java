cd8077b47f860020c347292d89d255e918bcb5b9
Blundell <paul@novoda.com>
2016-10-14 16:41:43 +0100

    play winning position on the right and bottom

--- dojo-misc/battle-kata-tic-tac-toe/paul-16-10-14/the-windjammer/Main.java
+++ dojo-misc/battle-kata-tic-tac-toe/paul-16-10-14/the-windjammer/Main.java
@@ -9,10 +9,58 @@ public class Main {
      */
     public static void main(String[] args) {
         String[] board = args[0].split(",");
-        System.out.println(getPosition(board));
+        String key = args[1];
+        int positionToPlay = getWinningMove(key, board);
+
+        if (positionToPlay == NO_AVAILABLE_MOVE) {
+            positionToPlay = getAnyOpenPosition(board);
+        }
+
+        System.out.println(positionToPlay);
     }
 
-    static int getPosition(String[] board) {
+    private static int getWinningMove(String key, String[] board) {
+
+        if (board[0].equals(key)) {
+            if (board[1].equals(key)) {
+                return 2;
+            }
+        }
+
+        if (board[3].equals(key)) {
+            if (board[4].equals(key)) {
+                return 5;
+            }
+        }
+
+        if (board[6].equals(key)) {
+            if (board[7].equals(key)) {
+                return 8;
+            }
+        }
+
+        if (board[0].equals(key)) {
+            if (board[3].equals(key)) {
+                return 6;
+            }
+        }
+
+        if (board[1].equals(key)) {
+            if (board[4].equals(key)) {
+                return 8;
+            }
+        }
+
+        if (board[2].equals(key)) {
+            if (board[5].equals(key)) {
+                return 8;
+            }
+        }
+
+        return NO_AVAILABLE_MOVE;
+    }
+
+    static int getAnyOpenPosition(String[] board) {
         for (int i = 0; i < board.length; i++) {
             String position = board[i];
             if (position.equals("-")) {
