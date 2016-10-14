1a55455f306c5a4209ae9dd6675cff6a5449f092
Ryan Feline <ryan.feline@novoda.com>
2016-10-14 16:57:40 +0100

    Random WHAT!

--- dojo-misc/battle-kata-tic-tac-toe/ryry/src/com/ryry/Main.java
+++ dojo-misc/battle-kata-tic-tac-toe/ryry/src/com/ryry/Main.java
@@ -1,9 +1,12 @@
 package com.ryry;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("0");
+        Random random = new Random();
+        System.out.println(random.ints(0, 8));
     }
 
 }
--- dojo-misc/battle-kata-tic-tac-toe/ryry/src/com/ryry/MainTest.java
+++ dojo-misc/battle-kata-tic-tac-toe/ryry/src/com/ryry/MainTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class MainTest extends TestCase {
 
-    public void testgiven() throws Exception {
+    public void testThatGivenIPlayAtAPositionWhenCheckingThatPositionThenItHasBeenPlayed() throws Exception {
         Board board = new Board();
 
         board.playMoveAt(0, Player.PLAYER_ONE);
