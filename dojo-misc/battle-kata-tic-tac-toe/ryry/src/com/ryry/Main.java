39d303c5ca1410ef76a7654bf903368b2d1c5368
Ryan Feline <ryan.feline@novoda.com>
2016-10-14 17:07:28 +0100

    More random stuff without running code...

--- dojo-misc/battle-kata-tic-tac-toe/ryry/src/com/ryry/Main.java
+++ dojo-misc/battle-kata-tic-tac-toe/ryry/src/com/ryry/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Random random = new Random();
-        System.out.println(random.ints(0, 8));
+        System.out.println("" + random.ints(0, 8));
     }
 
 }
