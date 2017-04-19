3679046424bc62bd35b761c28009a535f9262af8
amlcurran <aml.curran@gmail.com>
2017-04-19 18:12:45 +0200

    Mimic SharedPreferences better

--- dependency-inversion-android/lib/src/main/java/com/example/Preferences.java
+++ dependency-inversion-android/lib/src/main/java/com/example/Preferences.java
@@ -1,15 +1,19 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Preferences {
 
     public static final Preferences instance = new Preferences();
+    private final Map<String, String> cache = new HashMap<>();
 
     String valueForKey(String key) {
-        if (key.equals("lastFruit")) {
-            return "Banana";
-        } else {
-            return null;
-        }
+        return cache.get(key);
+    }
+
+    void put(String key, String value) {
+        cache.put(key, value);
     }
 
 }
