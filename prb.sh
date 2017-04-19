cc5c4374cb86682098a3769c040259e5461b7c07
Antonio Bertucci <archano@gmail.com>
2017-04-20 00:42:15 +0200

    Add print to prb script to  separate execution for each kata module

--- prb.sh
+++ prb.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "\n######## Build anagrams/ ########"
 anagrams/gradlew -p anagrams clean test
+
+echo "\n######## Build bankAccount/ ########"
 bankAccount/gradlew -p bankAccount clean test
 
