51059680400060319661b8bf16447a7031562dbb
Xavi Rigau <xavi@novoda.com>
2017-05-08 17:08:09 +0200

    Add silent kata: Luis, Paul and Xavi

---
+++ silent-kata-rul/luis-paul-xavi/src/test/java/com/novoda/rul/RecentlyUsedListTest.java
@@ -0,0 +1,14 @@
+package com.novoda.rul;
+
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RecentlyUsedListTest {
+
+    @Test
+    public void shouldRunUnitTests() {
+        assertTrue(true);
+    }
+
+}
