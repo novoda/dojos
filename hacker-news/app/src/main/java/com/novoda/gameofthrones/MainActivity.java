4aa3fb7bc658cb984a212110081a21a8370bcf76
Joe Timmins <joe@novoda.com>
2017-07-24 15:33:53 +0100

    Carly's solution to the Game of Thrones pairing exercise

--- hacker-news/app/src/main/java/com/novoda/gameofthrones/MainActivity.java
+++ hacker-news/app/src/main/java/com/novoda/gameofthrones/MainActivity.java
@@ -1,5 +1,7 @@
 package com.novoda.gameofthrones;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +9,11 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +24,43 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final List<Character> characterList = CharactersProvider.getCharacters();
+
         RecyclerView storiesView = (RecyclerView) findViewById(R.id.stories_view);
+
+        RecyclerView.Adapter adapter = new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                CharacterView characterView = new CharacterView(MainActivity.this);
+                return new GenericViewHolder(characterView);
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                CharacterView characterView = (CharacterView) holder.itemView;
+                TextView nameView = characterView.getCharacterNameView();
+                final Character character = characterList.get(position);
+                String name = character.getName();
+                nameView.setText(name);
+
+                characterView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(character.getWikipediaUrl()));
+                        MainActivity.this.startActivity(browserIntent);
+                    }
+                });
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return characterList.size();
+            }
+        };
+
+        storiesView.setAdapter(adapter);
+
         storiesView.setLayoutManager(new LinearLayoutManager(this));
     }
 
