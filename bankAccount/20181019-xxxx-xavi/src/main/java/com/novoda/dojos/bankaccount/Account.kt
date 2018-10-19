b3d85c7c1a68747c1d3a2b22963d9710ddfcf4fd
Xavi Rigau <xavi@novoda.com>
2018-10-19 17:49:05 +0200

    Create the deposit method

--- bankAccount/20181019-xxxx-xavi/src/main/java/com/novoda/dojos/bankaccount/Account.kt
+++ bankAccount/20181019-xxxx-xavi/src/main/java/com/novoda/dojos/bankaccount/Account.kt
@@ -1,3 +1,18 @@
 package com.novoda.dojos.bankaccount
 
-class Account
+class Account {
+
+    private val transactions: MutableList<Amount> = mutableListOf(Amount(0))
+
+    fun balance(): Balance = Balance(transactions.reduce { acc, amount -> acc + amount })
+
+    fun deposit(amount: Int) {
+        transactions.add(Amount(amount))
+    }
+}
+
+data class Balance(val amount: Amount)
+
+data class Amount(val value: Int) {
+    operator fun plus(other: Amount) = Amount(value + other.value)
+}
--- bankAccount/20181019-xxxx-xavi/src/test/java/com/novoda/dojos/bankaccount/AccountTest.kt
+++ bankAccount/20181019-xxxx-xavi/src/test/java/com/novoda/dojos/bankaccount/AccountTest.kt
@@ -1,13 +1,32 @@
 package com.novoda.dojos.bankaccount
 
-import com.nhaarman.mockito_kotlin.mock
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class AccountTest {
 
+    private val account = Account()
+
     @Test
-    fun `should be true`() {
-        val somethingKotlin = mock<Account>()
-        assert(somethingKotlin != null)
+    fun `balance should be 0 when no deposits`() {
+        account.assertBalanceEquals(0)
     }
+
+    @Test
+    fun `balance should be 10 when depositing 10`() {
+        account.deposit(10)
+
+        account.assertBalanceEquals(10)
+    }
+
+    @Test
+    fun `balance should be 30 when depositing 10 and 20`() {
+        account.deposit(10)
+        account.deposit(20)
+
+        account.assertBalanceEquals(30)
+    }
+
+    private fun Account.assertBalanceEquals(value: Int) = assertThat(balance().amount.value, equalTo(value))
 }
