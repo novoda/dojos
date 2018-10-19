af35051c1ed7c321f195a01f67830acb365225d5
Xavi Rigau <xavi@novoda.com>
2018-10-19 18:19:49 +0200

    Refactor account to have Transactions

--- bankAccount/20181019-xxxx-xavi/src/main/java/com/novoda/dojos/bankaccount/Account.kt
+++ bankAccount/20181019-xxxx-xavi/src/main/java/com/novoda/dojos/bankaccount/Account.kt
@@ -2,18 +2,18 @@ package com.novoda.dojos.bankaccount
 
 class Account {
 
-    private val transactions: MutableList<Amount> = mutableListOf(Amount(0))
+    private val transactions: MutableList<Transaction> = mutableListOf()
 
-    fun balance(): Balance = Balance(transactions.reduce { acc, amount ->
-        acc + amount
+    fun balance(): Balance = Balance(transactions.fold(Amount(0)) { acc, transaction ->
+        transaction.applyTo(acc)
     })
 
     fun deposit(amount: Amount) {
-        transactions.add(amount)
+        transactions.add(Deposit(amount))
     }
 
     fun withdraw(amount: Amount) {
-        transactions.add(Amount(-amount.value))
+        transactions.add(Withdrawal(amount))
     }
 }
 
@@ -21,4 +21,17 @@ data class Balance(val amount: Amount)
 
 data class Amount(val value: Int) {
     operator fun plus(other: Amount) = Amount(value + other.value)
+    operator fun minus(other: Amount) = Amount(value - other.value)
+}
+
+sealed class Transaction {
+    abstract fun applyTo(amount: Amount): Amount
+}
+
+data class Deposit(val transactionAmount: Amount) : Transaction() {
+    override fun applyTo(amount: Amount): Amount = amount + transactionAmount
+}
+
+data class Withdrawal(val transactionAmount: Amount) : Transaction() {
+    override fun applyTo(amount: Amount): Amount = amount - transactionAmount
 }
