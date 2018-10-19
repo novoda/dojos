81217fca271a92a75756aa570ac46b942b4d9fc9
Xavi Rigau <xavi@novoda.com>
2018-10-19 17:17:42 +0200

    Add initial set up

---
+++ bankAccount/20181019-xxxx-xavi/src/main/java/com/novoda/dojos/bankaccount/Something.java
@@ -0,0 +1,9 @@
+package com.novoda.dojos.bankaccount;
+
+
+public class Something {
+
+    public boolean isItTrue() {
+        return true;
+    }
+}
---
+++ bankAccount/20181019-xxxx-xavi/src/test/java/com/novoda/dojos/bankaccount/SomethingKotlinTest.kt
@@ -0,0 +1,13 @@
+package com.novoda.dojos.bankaccount
+
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.Test
+
+class SomethingKotlinTest {
+
+    @Test
+    fun `should be true`() {
+        val somethingKotlin = mock<SomethingKotlin>()
+        assert(somethingKotlin != null)
+    }
+}
