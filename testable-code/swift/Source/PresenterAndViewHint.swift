afc3c336ae88d947b7cc67ae065cd2d66cb0a2f2
Alex Curran <alex@novoda.com>
2018-11-16 13:21:05 +0100

    Should be everything needed for the kata

---
+++ testable-code/swift/Source/PresenterAndViewHint.swift
@@ -0,0 +1,32 @@
+//
+//  PresenterAndView.swift
+//  Testable codeTests
+//
+//  Created by Alex Curran on 16.11.18.
+//  Copyright © 2018 Novoda. All rights reserved.
+//
+
+import Foundation
+
+// if you get stuck, or don't know MVP very well, you can start with these hints to help you out.
+// once you've extracted some methods out of the big bunch of code in ViewController.swift,
+// think about which of those methods makes sense to be in a view (e.g. anything that displays
+// anything), and what should be in a presenter (in this simple project, anything else :D )
+
+//protocol View: class {
+//
+//}
+//
+//class Presenter {
+//
+//    private weak var view: View?
+//
+//    init(view: View) {
+//        self.view = view
+//    }
+//
+//    func beginPresenting() {
+//        // this should probably do something...
+//    }
+//
+//}
