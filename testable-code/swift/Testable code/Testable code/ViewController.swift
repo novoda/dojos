04e4dcd8db56be4e4e68e44bca2154318a13907d
Alex Curran <alex@novoda.com>
2018-11-16 10:17:15 +0100

    Adds basic project structure

---
+++ testable-code/swift/Testable code/Testable code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testable code
+//
+//  Created by Alex Curran on 16.11.18.
+//  Copyright © 2018 Novoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
