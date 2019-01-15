3dc76e463dfe370483e45d5022b5b44060446888
Tobias Heine <tobias@novoda.com>
2019-01-15 10:08:42 +0100

    Initial workshop commit

---
+++ workshops/AndroidKotlinWorkshop/app/src/main/java/com/novoda/workshop/ContributorsActivity.kt
@@ -0,0 +1,13 @@
+package com.novoda.workshop
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ContributorsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contributers)
+    }
+    
+}
