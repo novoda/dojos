5e53d6a408f5b6d00108ec96aa83f0aaa1ec0333
Tobias Heine <tobias@novoda.com>
2019-01-16 14:20:26 +0100

    Add launcher layout

---
+++ workshops/AndroidKotlinWorkshop/app/src/main/java/com/novoda/workshop/LauncherActivity.kt
@@ -0,0 +1,13 @@
+package com.novoda.workshop
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+internal class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
